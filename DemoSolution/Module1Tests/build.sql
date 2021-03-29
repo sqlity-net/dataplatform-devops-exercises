aea99c550dd9f94496e0a4afed92c62045ea913c
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-03-28 23:40:05 -0400

    add todos

--- DemoSolution/Module1Tests/build.sql
+++ DemoSolution/Module1Tests/build.sql
@@ -1,3 +1,10 @@
+:!! echo ---------------------------------- TODO ----------------------------------
+:!! echo 1. add ":r" in front of file names in installalltests.sql file
+:!! echo 2. move unzipped tSQLt artifacts to subfolder in the tSQLt project in the Demo Solution
+:!! echo 3. Add explanatory print statements so that we know what is going on during the build
+:!! echo 4. Add run build script to tools menu
+:!! echo  -------------------------------------------------------------------------
+
 :r $(DEMOSOLUTIONPATH)\Module1Project\build.sql
 GO
 
