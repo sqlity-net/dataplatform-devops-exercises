3c7ff752747e00461554466ebd712575913650af
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-03-29 22:39:14 -0400

    Add gitignore and update the solution to include all the projects

--- DemoSolution/Module1Tests/build.sql
+++ DemoSolution/Module1Tests/build.sql
@@ -1,5 +1,4 @@
 :!! echo ---------------------------------- TODO ----------------------------------
-:!! echo 1. add ":r" in front of file names in installalltests.sql file
 :!! echo 2. move unzipped tSQLt artifacts to subfolder in the tSQLt project in the Demo Solution
 :!! echo 3. Add explanatory print statements so that we know what is going on during the build
 :!! echo 4. Add run build script to tools menu
@@ -8,13 +7,10 @@
 :r $(DEMOSOLUTIONPATH)\Module1Project\build.sql
 GO
 
-:!! del $(DEMOSOLUTIONPATH)\Module1Tests\installalltests.sql >nul 2>&1
-:OUT $(DEMOSOLUTIONPATH)\Module1Tests\installalltests.sql
-:!! DIR $(DEMOSOLUTIONPATH)\Module1Tests\*.class.sql /b/o
-:OUT stdout
-GO
+:!! pushd $(DEMOSOLUTIONPATH)\Module1Tests & (for /r %i in (*.class.sql) do @echo :r "%~i" & echo GO) >installalltests.sql & popd
 
 :r $(DEMOSOLUTIONPATH)\tSQLt\build.sql
 GO
 
 :r $(DEMOSOLUTIONPATH)\Module1Tests\installalltests.sql
+GO
