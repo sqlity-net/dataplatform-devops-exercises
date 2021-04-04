91375ae3b2b8b8466e0549f747709c524c0e20aa
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-04-03 21:05:09 -0400

    Cleaned up connections and removed an extra test test file

--- Module1-Exercise1/M1-Exercise1-Tests/personreporttests.class.sql
+++ Module1-Exercise1/M1-Exercise1-Tests/personreporttests.class.sql
@@ -1,2 +1,8 @@
-PRINT 'got to personreporttests.class.sql';
+EXEC tSQLt.NewTestClass 'personreporttests';
+GO
+CREATE PROCEDURE personreporttests.[test something]
+AS
+BEGIN
+	EXEC tSQLt.Fail 'TO DO';
+END;
 GO
