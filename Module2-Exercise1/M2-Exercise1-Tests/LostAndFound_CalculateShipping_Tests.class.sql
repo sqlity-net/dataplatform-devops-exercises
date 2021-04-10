23b6c1c24a5880c6645c83d28761a9209b405e6b
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-04-10 13:16:16 -0400

    I wonder what state the repo is in now.

---
+++ Module2-Exercise1/M2-Exercise1-Tests/LostAndFound_CalculateShipping_Tests.class.sql
@@ -0,0 +1,9 @@
+EXEC tSQLt.NewTestClass 'LostAndFound_CalculateShipping_Tests';
+GO
+
+CREATE PROCEDURE LostAndFound_CalculateShipping_Tests.[test something]
+AS
+BEGIN
+  EXEC tSQLt.Fail 'TODO';
+END
+GO
