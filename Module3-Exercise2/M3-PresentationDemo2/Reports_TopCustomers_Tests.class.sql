cedc57747a17624cf887d70787bd2efa0d9c8e4b
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-04-17 21:44:39 -0400

    Added Module3-Exercise2 and some minor clean up

---
+++ Module3-Exercise2/M3-PresentationDemo2/Reports_TopCustomers_Tests.class.sql
@@ -0,0 +1,19 @@
+EXEC tSQLt.NewTestClass 'Reports_TopCustomers_Tests';
+GO
+CREATE PROCEDURE Reports_TopCustomers_Tests.[test returns the top 2 out of 3 customers with their reservation count]
+AS
+BEGIN
+  INSERT INTO Booking.Reservations(CustomerId)
+  VALUES(1001),(1001),(1001),(2002),(3003),(3003);
+  
+  SELECT CustomerId, ReservationCount INTO #Actual FROM Reports.TopNCustomers(2);
+  
+  SELECT TOP(0) A.* INTO #Expected FROM #Actual A RIGHT JOIN #Actual X ON 1=0;
+  
+  INSERT INTO #Expected
+  VALUES(1001,3),(3003,2);
+
+  EXEC tSQLt.AssertEqualsTable '#Expected','#Actual';
+
+END;
+GO
