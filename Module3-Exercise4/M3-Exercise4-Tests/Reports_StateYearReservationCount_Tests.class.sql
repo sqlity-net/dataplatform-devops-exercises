f9dab1b6a8efc989d0af55b4940ba58f73e63b0c
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-04-25 11:05:18 -0400

    Undelete! Better instructions in the README.md

--- Module3-Exercise4/M3-Exercise4-Tests/Reports_StateYearReservationCount_Tests.class.sql
+++ Module3-Exercise4/M3-Exercise4-Tests/Reports_StateYearReservationCount_Tests.class.sql
@@ -1,8 +1,2 @@
 EXEC tSQLt.NewTestClass 'Reports_StateYearReservationCount_Tests';
 GO
-CREATE PROCEDURE Reports_StateYearReservationCount_Tests.[test shell test] 
-AS
-BEGIN
-  EXEC tSQLt.Fail 'TODO';
-END
-GO;
